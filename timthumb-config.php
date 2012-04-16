7973a67efd6cdc8290f0eb0262c57b1f6e5ee8cc
bkmacdaddy <bkmacdaddy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-16 18:30:16 +0000

    adding js files
    git-svn-id: https://plugins.svn.wordpress.org/pinterest-rss-widget/tags/1.2.3@531958 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ timthumb-config.php
@@ -0,0 +1,4 @@
+<?php
+if(! defined('FILE_CACHE_DIRECTORY') ) define ('FILE_CACHE_DIRECTORY', '../../uploads/prw_tmp');	
+// Directory where images are cached. Left blank it will use the system temporary directory (which is better for security)
+?>
